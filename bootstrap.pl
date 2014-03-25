1a770cc0e3d807e4c6a38e032122a5ce65281691
Alexander Smirnov <zoocide@gmail.com>
2014-03-25 18:24:33 +0400

    bootstrap: use BasicTools

--- bootstrap.pl
+++ bootstrap.pl
@@ -1,23 +1,31 @@
 #!/usr/bin/perl -w
 use strict;
+use Cwd;
 use Config;
 use File::Spec::Functions qw(rel2abs catfile);
+use BasicTools;
+use constant windoze => $^O eq 'MSWin32';
 
-my $wget = 'wget';
-my $wget_pipe = "wget -O -";
-my @tar = qw(tar -xf);
-my $make = 'make';
 my $perl = $^X;
 
 my $install_dir = $ARGV[0] ? rel2abs($ARGV[0]) : '';
 my $prefix      = $install_dir ? 'PREFIX='.$install_dir : '';
+my $temp_dir    = 'bootstrap_temp';
 
 ## set environment variables ##
 require my_lib;
 my_lib->import($install_dir);
 
+## initialize BasicTools ##
+my $bt = BasicTools->new;
+$bt->init_tools;
+$bt->has_make || !windoze || $bt->get_dmake($install_dir);
+
 #sub my_system{ print join( ' ', @_, "\n"); 0 }
 sub my_system{ system @_ }
+
+## create $temp_dir ##
+mkdir $temp_dir;
 
 ## install ExtUtils::MakeMaker ##
 eval{ require ExtUtils::MakeMaker };
@@ -49,23 +57,32 @@ sub get_and_install_dist
 {
   my $url = shift;
   $url =~ m#.*/((.*)\.tar\.gz)$# or die "can`t parse url '$url'\n";
-  my ($fname, $dir) = ($1, $2);
+  my ($fname, $dir) = map catfile($temp_dir, $_), ($1, $2);
 
-  !-f $fname && my_system($wget, $url)     && die "can`t download file '$url'\n";
-  !-d $dir   && my_system(@tar, $fname)    && die "can`t extract file '$fname'\n";
+  ## download package ##
+  !-f $fname && $bt->download($url, $temp_dir);
+
+  ## extract package ##
+  !-d $dir   && $bt->extract($fname);
+
+  ## install package ##
+  my $cd = cwd();
   chdir($dir)                              || die "can`t change directory '$dir'\n";
   my_system($perl, 'Makefile.PL', $prefix) && die "can`t create makefile\n";
-  my_system($make)                         && die "make: failed\n";
-  my_system($make, 'install')              && die "make install: failed\n";
-  chdir('..');
+  $bt->make;
+  $bt->make('install');
+  chdir($cd);
   print "## $dir successfully installed ##\n";
 }
 
 sub install_online_cpanm
 {
   my $url = 'http://cpanmin.us';
-  my $inst_dir = $install_dir ? '-l '.$install_dir : '';
-  my_system("$wget_pipe $url | $perl - $inst_dir App::cpanminus") && die "can`t install cpanminus\n";
+  my @opts = qw(App::cpanminus);
+  my $cpanm = catfile($temp_dir, 'cpanm');
+  $install_dir && unshift @opts, '-l', $install_dir;
+  $bt->download($url, $cpanm);
+  my_system($perl, $cpanm, @opts) && die "can`t install cpanminus\n";
   print "## cpanminus successfully installed ##\n";
 }
 
